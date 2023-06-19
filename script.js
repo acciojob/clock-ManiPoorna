f69279743238fff99b1e917846f269a9c65f580f
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-06-19 22:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const date  = new Date();
-const displayDate = date.getFullYear() +'-'+ (date.getMonth()+1) +'-'+date.getDate(); 
+const displayDate = date.getFullYear() +'-'+ (date.getMonth()+1) +'-'+date.getDate()+","; 
 document.getElementById("date").innerHTML = displayDate;
 const displayTime = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-document.getElementById("time").innerHTML = displayTime;
+document.getElementById("timer").innerHTML = displayTime;
